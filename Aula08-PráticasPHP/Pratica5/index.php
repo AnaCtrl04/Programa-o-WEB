79740284d4ad5835c048e0d341cc11b43a68e565
AnaCtrl04 <carol.anapm2004@gmail.com>
2024-10-06 15:04:16 -0300

    pratica5

--- Aula08-PráticasPHP/Pratica5/index.php
+++ Aula08-PráticasPHP/Pratica5/index.php
@@ -1,7 +1,7 @@
 <?php
 $disciplinas = array("Banco de Dados II", "Estrutura de dados II", "Administração e Sistemas", "Engenharia da computação II", "Desenvolvimento WEB I");
 
-$professores = array("n lembro", "Fernando bastos", "area lcoal", "n lembro 2", "Cleber Nardelli");
+$professores = array("Marco B.", "Fernando Bastos", "Sandro", "Jullian", "Cleber Nardelli");
 
 for ($i = 0; $i < 5; $i++) {
     echo "Disciplina: " . $disciplinas[$i] . ", Professor: " . $professores[$i] . ".<br>";

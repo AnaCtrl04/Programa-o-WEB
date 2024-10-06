bf7acb10eade15140606d0245b1c8ee281f1a47e
AnaCtrl04 <carol.anapm2004@gmail.com>
2024-10-06 14:59:05 -0300

    pratica 1,2,3,4

--- Aula08-PráticasPHP/Pratica2/index.php
+++ Aula08-PráticasPHP/Pratica2/index.php
@@ -1,19 +1,17 @@
 <?php
 
-//
+//parte 1 da pratica
 $salario1 = 1000;
 $salario2 = 2000;
 
-//
 $salario1 = $salario2;
 
-//
 $salario2 = $salario2 + 1;
 
-//
 $salario1 = $salario1 * 1.10;
 
-//
+
+//parte 2 da pratica
 echo "Salario 1: $salario1 e Salario 2: $salario2 <br>";
 
 if ($salario1 > $salario2) {
@@ -27,10 +25,11 @@ if ($salario1 > $salario2) {
 }
 echo '<br><br><br>';
 
-/*for ($let = 1; $let <= 100; $let++) {
+//parte 3 da pratica
+for ($let = 1; $let <= 100; $let++) {
     $salario1 += 1;
     if($let == 50){
         break;
     }
     echo"O número é $let <br>"; 
-}*/
+}

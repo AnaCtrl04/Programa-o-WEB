c3bdaa84f840b61f990849499726d76ec2637e00
AnaCtrl04 <carol.anapm2004@gmail.com>
2024-10-06 14:45:16 -0300

    pratica2

--- Aula08-PráticasPHP/Pratica2/index.php
+++ Aula08-PráticasPHP/Pratica2/index.php
@@ -1,25 +1,25 @@
 <?php
 
 //
-$first_salary = 1000;
-$second_salary = 2000;
+$salario1 = 1000;
+$salario2 = 2000;
 
 //
-$second_salary = $first_salary;
+$salario1 = $salario2;
 
 //
-$second_salary = $second_salary + 1;
+$salario2 = $salario2 + 1;
 
 //
-$first_salary = $first_salary * 1.10;
+$salario1 = $salario1 * 1.10;
 
 //
-echo "Salario 1: $first_salary e Salario 2: $second_salary <br>";
+echo "Salario 1: $salario1 e Salario 2: $salario2 <br>";
 
-if ($first_salary > $second_salary) {
+if ($salario1 > $salario2) {
     echo 'O salario 1 é maior do que o salario 2';
 } else {
-    if ($first_salary < $second_salary) {
+    if ($salario1 < $salario2) {
         echo 'O salario 2 é maior do que o salario 1';
     } else {
         echo 'Os dois salários sãoiguais';
@@ -27,10 +27,10 @@ if ($first_salary > $second_salary) {
 }
 echo '<br><br><br>';
 
-for ($let = 1; $let <= 100; $let++) {
-    $first_salary += 1;
+/*for ($let = 1; $let <= 100; $let++) {
+    $salario1 += 1;
     if($let == 50){
         break;
     }
     echo"O número é $let <br>"; 
-}
+}*/
